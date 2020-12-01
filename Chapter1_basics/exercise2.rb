a7bd815a242ca97c57978d180ac7d2cde9fb398b
Salo M <salofb21410@gmail.com>
2020-12-01 18:49:56 +1300

    exercise 2

---
+++ Chapter1_basics/exercise2.rb
@@ -0,0 +1,15 @@
+thousands = 2019 / 1000
+
+puts "thousands: #{thousands}"
+
+hundreds = 2019 % 1000 / 100
+
+puts "hundreds: #{hundreds}"
+
+tens = 2019 % 100 / 10 
+
+puts "tens: #{tens}"
+
+ones = 2019 % 10 
+
+puts "ones: #{ones}"
